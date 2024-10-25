ff77d8ceced335be19b24f0e29a4b8b4392b75db
jurabek10 <samiev.jurabek@bk.ru>
2024-10-26 00:44:03 +0900

    feat: integration environmental variable

--- apps/ease-up-batch/src/ease-up-batch.module.ts
+++ apps/ease-up-batch/src/ease-up-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EaseUpBatchController } from './ease-up-batch.controller';
 import { EaseUpBatchService } from './ease-up-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [EaseUpBatchController],
   providers: [EaseUpBatchService],
 })
