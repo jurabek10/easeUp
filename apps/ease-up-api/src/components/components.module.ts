d990c66fe6effdcc2357a2ada2a97c224066761b
jurabek10 <samiev.jurabek@bk.ru>
2024-10-26 02:21:41 +0900

    feat: create schema models and enums of member

--- apps/ease-up-api/src/components/components.module.ts
+++ apps/ease-up-api/src/components/components.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PlaceModule } from './place/place.module';
+import { AuthModule } from './auth/auth.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { CommentModule } from './comment/comment.module';
+import { FollowModule } from './follow/follow.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
 
 @Module({
-  imports: [MemberModule, PlaceModule]
+  imports: [MemberModule, PlaceModule, AuthModule, BoardArticleModule, CommentModule, FollowModule, LikeModule, ViewModule]
 })
 export class ComponentsModule {}
