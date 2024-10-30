e173f10811194c29c63f22711a1d3bd7ee08af03
jurabek10 <samiev.jurabek@bk.ru>
2024-10-30 09:48:20 +0900

    feat: integration of Authentication and Autheration

--- apps/ease-up-api/src/components/auth/guards/auth.guard.ts
+++ apps/ease-up-api/src/components/auth/guards/auth.guard.ts
@@ -11,10 +11,12 @@ export class AuthGuard implements CanActivate {
 			const request = context.getArgByIndex(2).req;
 			const bearerToken = request.headers.authorization;
 			if (!bearerToken) throw new BadRequestException(Message.TOKEN_NOT_EXIST);
+			// console.log('bearer Token => ', bearerToken);
 			const token = bearerToken.split(' ')[1],
 				authMember = await this.authService.verifyToken(token);
 			if (!authMember) throw new UnauthorizedException(Message.NOT_AUTHENTICATED);
 			console.log('memberNick[auth] =>', authMember.memberNick);
+
 			request.body.authMember = authMember;
 			return true;
 		}
