6781e4ade82898ef933a6ba01006cf6a2251794c
jurabek10 <samiev.jurabek@bk.ru>
2024-11-05 03:18:17 +0900

    feat: createBoardArticle and getBoardArticle api

--- apps/ease-up-api/src/components/board-article/board-article.resolver.ts
+++ apps/ease-up-api/src/components/board-article/board-article.resolver.ts
@@ -1,4 +1,35 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { BoardArticleService } from './board-article.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMogoObjectId } from '../../libs/config';
 
 @Resolver()
-export class BoardArticleResolver {}
+export class BoardArticleResolver {
+	constructor(private readonly boardArticleService: BoardArticleService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => BoardArticle)
+	public async createBoardArticle(
+		@Args('input') input: BoardArticleInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Mutation: createBoardArticle');
+		return await this.boardArticleService.createBoardArticle(memberId, input);
+	}
+	@UseGuards(WithoutGuard)
+	@Query((returns) => BoardArticle)
+	public async getBoardArticle(
+		@Args('articleId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Query: getBoardArticle');
+		const articleId = shapeIntoMogoObjectId(input);
+		return await this.boardArticleService.getBoardArticle(memberId, articleId);
+	}
+}
