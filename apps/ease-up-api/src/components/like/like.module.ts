e4368df5ae9ebc8a3300eb5f5de2cce1758b6f0a
jurabek10 <samiev.jurabek@bk.ru>
2024-11-06 01:21:46 +0900

    feat: develop resolver and service of likeModule

--- apps/ease-up-api/src/components/like/like.module.ts
+++ apps/ease-up-api/src/components/like/like.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { LikeResolver } from './like.resolver';
+import { LikeService } from './like.service';
+import LikeSchema from '../../schemas/Like.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
 
-@Module({})
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'Like', schema: LikeSchema }])],
+	providers: [LikeResolver, LikeService],
+	exports: [LikeService],
+})
 export class LikeModule {}
