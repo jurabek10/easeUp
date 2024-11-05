0405c100170f9f9e3f3b155a9b48e97d6e7db3bf
jurabek10 <samiev.jurabek@bk.ru>
2024-11-06 01:02:09 +0900

    feat: develop createComment, updateComment and getComments APIs

--- apps/ease-up-api/src/components/comment/comment.module.ts
+++ apps/ease-up-api/src/components/comment/comment.module.ts
@@ -4,17 +4,16 @@ import { CommentResolver } from './comment.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import CommentSchema from '../../schemas/Comment.model';
 import { AuthModule } from '../auth/auth.module';
-import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
-import { Property } from '../../libs/dto/property/property';
 import { BoardArticleModule } from '../board-article/board-article.module';
+import { PropertyModule } from '../property/property.module';
 
 @Module({
 	imports: [
 		MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
 		AuthModule,
 		MemberModule,
-		Property,
+		PropertyModule,
 		BoardArticleModule,
 	],
 	providers: [CommentResolver, CommentService],
