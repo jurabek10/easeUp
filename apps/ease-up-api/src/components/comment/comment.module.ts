8d7b759524c70b841ba1e0e387a5c21b230dd0dc
jurabek10 <samiev.jurabek@bk.ru>
2024-11-06 00:37:40 +0900

    feat: configure comment module

--- apps/ease-up-api/src/components/comment/comment.module.ts
+++ apps/ease-up-api/src/components/comment/comment.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentResolver } from './comment.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import CommentSchema from '../../schemas/Comment.model';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
+import { MemberModule } from '../member/member.module';
+import { Property } from '../../libs/dto/property/property';
+import { BoardArticleModule } from '../board-article/board-article.module';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
+		AuthModule,
+		MemberModule,
+		Property,
+		BoardArticleModule,
+	],
+	providers: [CommentResolver, CommentService],
+})
 export class CommentModule {}
