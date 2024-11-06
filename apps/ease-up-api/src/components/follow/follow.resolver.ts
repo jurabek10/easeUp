4303e635215711222cd797789f2b5322cb22bbbb
jurabek10 <samiev.jurabek@bk.ru>
2024-11-07 01:37:17 +0900

    feat: develop subscribe and unsubscribe apis

--- apps/ease-up-api/src/components/follow/follow.resolver.ts
+++ apps/ease-up-api/src/components/follow/follow.resolver.ts
@@ -1,7 +1,29 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { Follower } from '../../libs/dto/follow/follow';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { shapeIntoMogoObjectId } from '../../libs/config';
 
 @Resolver()
 export class FollowResolver {
 	constructor(private readonly followService: FollowService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Follower)
+	public async subscribe(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Follower> {
+		console.log('Mutation: subscribe');
+		const followingId = shapeIntoMogoObjectId(input);
+		return await this.followService.subscribe(memberId, followingId);
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Follower)
+	public async unsubscribe(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Follower> {
+		console.log('Mutation:  unsubscribe');
+		const followingId = shapeIntoMogoObjectId(input);
+		return await this.followService.unsubscribe(memberId, followingId);
+	}
 }
