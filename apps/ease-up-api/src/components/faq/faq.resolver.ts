8b8d911d2c0a5343b4b905aab0aa572f850d038d
jurabek10 <samiev.jurabek@bk.ru>
2024-12-04 22:56:04 +0900

    feat: develop getFaqs business logic

--- apps/ease-up-api/src/components/faq/faq.resolver.ts
+++ apps/ease-up-api/src/components/faq/faq.resolver.ts
@@ -4,10 +4,11 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { UseGuards } from '@nestjs/common';
 import { RolesGuard } from '../auth/guards/roles.guard';
-import { Faq } from '../../libs/dto/faq/faq';
-import { FaqInput } from '../../libs/dto/faq/faq.input';
+import { Faq, Faqs } from '../../libs/dto/faq/faq';
+import { AllFaqsInquiry, FaqInput } from '../../libs/dto/faq/faq.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class FaqResolver {
@@ -20,5 +21,12 @@ export class FaqResolver {
 		console.log('Mutation: createFaq');
 		input.memberId = memberId;
 		return await this.faqService.createFaq(input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Faqs)
+	public async getAllFaqs(@Args('input') input: AllFaqsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Faqs> {
+		console.log('Query: getAllFaqs');
+		return await this.faqService.getAllFaqs(input);
 	}
 }
--- apps/ease-up-api/src/components/faq/faq.service.ts
+++ apps/ease-up-api/src/components/faq/faq.service.ts
@@ -1,9 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Faq } from '../../libs/dto/faq/faq';
-import { FaqInput } from '../../libs/dto/faq/faq.input';
-import { Message } from '../../libs/enums/common.enum';
+import { Model, ObjectId } from 'mongoose';
+import { Faq, Faqs } from '../../libs/dto/faq/faq';
+import { AllFaqsInquiry, FaqInput } from '../../libs/dto/faq/faq.input';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { T } from '../../libs/types/common';
+import { FaqStatus } from '../../libs/enums/faq.enum';
+import { lookupMember } from '../../libs/config';
 
 @Injectable()
 export class FaqService {
@@ -17,5 +20,32 @@ export class FaqService {
 			console.log('Error, Service.model:', err.message);
 			throw new BadRequestException(Message.CREATED_FAILED);
 		}
+	}
+
+	public async getAllFaqs(input: AllFaqsInquiry): Promise<Faqs> {
+		const { faqStatus, faqCategory } = input.search;
+		const match: T = {};
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+		if (faqStatus) match.faqStatus = faqStatus;
+		if (faqCategory) match.faqCategory = faqCategory;
+		const result = await this.faqModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
 	}
 }
