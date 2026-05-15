33ad65506b1e880cc2ce019ada05431bd856f68b
Samiev Jurabek <samievjurabek@Samievs-MacBook-Pro.local>
2026-05-15 22:10:05 +0900

    Handle HTTP requests in logging interceptor

--- apps/ease-up-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/ease-up-api/src/libs/interceptor/Logging.interceptor.ts
@@ -13,7 +13,7 @@ export class LoggingInterceptor implements NestInterceptor {
 		// this.logger.log(`TYPE ${requestType}`, 'REQUEST');
 
 		if (requestType === 'http') {
-			// Develop if needed
+			return next.handle();
 		} else if (requestType === 'graphql') {
 			// 1. Print Request
 			const gqlContext = GqlExecutionContext.create(context);
@@ -30,6 +30,8 @@ export class LoggingInterceptor implements NestInterceptor {
 				}),
 			);
 		}
+
+		return next.handle();
 	}
 
 	private stringify(context: ExecutionContext): string {
